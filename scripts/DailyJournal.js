723e0bb5a96cfec567c86665432262a4f570635a
Val Currie <contact@valcuri.email>
2021-04-21 19:23:24 -0500

    moved entry form from index.html to scripts/DailyJournal.js

--- scripts/DailyJournal.js
+++ scripts/DailyJournal.js
@@ -1,7 +1,12 @@
 import { Entries } from './Entries.js'
+import { JournalForm } from './JournalForm.js'
 
 export const DailyJournal = () => {
     return `
+		<form class="entryForm w3-card w3-container" action="">
+            ${ JournalForm() }
+		</form>
+
         <div class="noteList">
             ${ Entries() }
         </div>
