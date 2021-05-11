da169b79772dc92cfaab65ec1f1a15e0b7ea2bd7
Val Currie <contact@valcuri.email>
2021-05-10 23:49:29 -0500

    created a data access module and entries provider

---
+++ scripts/EntriesProvider.js
@@ -0,0 +1,45 @@
+import { entriesRaw } from "./dataAccess.js";
+
+export const getEntries = () => {
+  // replace mood ids with something we can print
+  return rawEntries.map((entry) => {
+    // first find our mood given the id from the entry object
+    const mood = moods.find((m) => m.id === entry.id);
+    // if the entry has an emoji, make sure to include the whole mood in the entry
+    entry.mood = "emoji" in mood ? mood : mood.label;
+    return entry;
+  });
+};
+export const getMoods = () => moods.map((m) => ({ ...m }));
+
+export const buildEntry = (subject, text, mood, date, timeSpent) => {
+  if (!subject || !text || !mood || !date || !timeSpent) {
+    return "missing fields";
+  } // TODO add more validation
+  entryBuilder.subject = subject;
+  entryBuilder.text = text;
+  entryBuilder.mood = moods.find((m) => m.label === mood).id;
+  entryBuilder.date = date;
+  entryBuilder.timeSpent = timeSpent;
+};
+
+const entryBuilder = {};
+const moods = [
+  {
+    id: 1,
+    label: "happy",
+  },
+  {
+    id: 2,
+    label: "sad",
+  },
+  {
+    id: 3,
+    label: "neutral",
+  },
+  {
+    id: 4,
+    label: "confused",
+    emoji: "😕",
+  },
+];
--- scripts/dataAccess.js
+++ scripts/dataAccess.js
@@ -4,6 +4,8 @@ const appState = {
   entries: [],
 };
 
+export const entriesRaw = () => appState.entries.map((e) => ({ ...e }));
+
 export const fetchData = () => {
   return fetch("http://localhost:8081/entries")
     .then((res) => {
@@ -31,10 +33,6 @@ export const saveJournalEntry = (newEntry) => {
   }).then(dispatchStateChanged());
 };
 
-export const getEntries = () => {
-  return [...appState.entries];
-};
-
 const embeddedEntries = [
   {
     id: 1,
@@ -42,7 +40,7 @@ const embeddedEntries = [
     text:
       "Declare objects with curly braces. Declare arrays with square brackets.",
     date: "April 15, 2021",
-    feeling: "happy",
+    mood: "happy",
     timeSpent: 15,
   },
   {
@@ -51,7 +49,7 @@ const embeddedEntries = [
     text:
       "Usually this means whatever you declare can't change -- the exception is when it is a reference to an object or array, which remain mutable.",
     date: "April 15, 2021",
-    feeling: "ok",
+    mood: "ok",
     timeSpent: 5,
   },
   {
@@ -59,7 +57,7 @@ const embeddedEntries = [
     subject: "javascript arrays all have a push() method",
     text: "So myArray.push(thing) would add thing to the end of myArray",
     date: "April 15, 2021",
-    feeling: "happy",
+    mood: "happy",
     timeSpent: 2,
   },
   {
@@ -68,7 +66,7 @@ const embeddedEntries = [
     text:
       "I couldn't find my external entries. You're currently looking at the default, embedded entries used for frontend testing.",
     date: "April 25, 2021",
-    feeling: "sad",
+    mood: "sad",
     timeSpent: 80,
   },
 ];
